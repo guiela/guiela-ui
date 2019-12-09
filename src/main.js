59c3b358ddbc9c326030590843b4dc6f4b382423
Anitche Chisom <anitchec.dev@gmail.com>
2019-12-09 08:31:25 +0000

    Install ckeditor5-vue.

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import BootstrapVue from 'bootstrap-vue'
+import CKEditor from '@ckeditor/ckeditor5-vue';
 
 Vue.use(BootstrapVue)
+Vue.use( CKEditor );
 
 Vue.config.productionTip = false
 
